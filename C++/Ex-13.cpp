3b3f55946e65b7d9e66aafcd77f3901ebd2d289c
MI0DL <m.lapchinskiy@gmai.com>
2024-12-05 18:44:14 +0300

    Фикс

--- C++/Ex-13.cpp
+++ C++/Ex-13.cpp
@@ -7,6 +7,15 @@ using namespace std;
 int main(){
     system("chcp 1251 > null");
     setlocale(LC_ALL, "Rus");
-    
+    int a = 100, b = 100, c = 100;
+    for (int i = 1; i <= a; i++){
+        for (int j = 1; j <= b; j++){
+            for (int k = 1; k <= c; k++){
+                if (i * i + j * j == k * k){
+                    cout << i << " + " << j << " = " << k << " "  << endl;
+                }
+            }
+        }
+    }
     return 0;
 }
