7c4067e75f8e629383a92ca0d756011e33e03c79
MI0DL <m.lapchinskiy@gmai.com>
2024-12-05 20:09:58 +0300

    Fisk

---
+++ C++/Ex-16.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+    system("chcp 1251 > null");
+    setlocale(LC_ALL, "Rus");
+    int IF_FIB;
+    cout << "Введите число от 1 от 10: ";
+    cin >> IF_FIB;
+    if (IF_FIB < 1){
+        cout << "Вы ввели число не диапозоне от 1 до 10 ";
+    }
+    else {
+        switch (IF_FIB){
+            case 1:
+                cout << "Число находиться в списке чисел Фибаначчи";
+                break;
+            case 2:
+                cout << "Число находиться в списке чисел Фибаначчи";
+                break;
+            case 3:
+                cout << "Число находиться в списке чисел Фибаначчи";
+                break;
+            case 5:
+                cout << "Число находиться в списке чисел Фибаначчи";
+                break;
+            case 8:
+                cout << "Число находиться в списке чисел Фибаначчи";
+                break;
+            default:
+                cout << "Число не находиться в списке чисел Фибаначчи";
+                break;
+        }
+    }
+    return 0;
+}
