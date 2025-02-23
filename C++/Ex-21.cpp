2eb30393e0a0e0a3c842175eae493ba19ef97957
MI0DL <m.lapchinskiy@gmai.com>
2025-02-23 17:24:03 +0300

    Test

--- C++/Ex-21.cpp
+++ C++/Ex-21.cpp
@@ -6,5 +6,15 @@ using namespace std;
 int main(){
     system("chcp 1251 > null");
     setlocale(LC_ALL, "rus");
-    
+    char symb;
+    int num;
+    char* p;
+    int* q;
+    p = &symb;
+    q = &num;
+    *p = 'A';
+    *q = 100;
+    cout << "symb = " << symb << endl;
+    cout  << "num = " << num << endl;
+    return 0;
 }
