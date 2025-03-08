e7c2f8384db7f42d167b1ed7df62c70879cd5ab7
MI0DL <m.lapchinskiy@gmai.com>
2025-03-08 21:32:08 +0300

    Test

--- C++/Test1.cpp
+++ C++/Test1.cpp
@@ -7,12 +7,6 @@ using namespace std;
 int main() {
     system("chcp 1251 > null");
     setlocale(LC_ALL, "Rus");
-    srand(time(NULL)); // Добавлена точка с запятой
-
-    int massiv[10];
-    for (int &n : massiv) { // Перебор элементов массива
-        n = rand() % 10 + 1; // Генерация случайного числа от 1 до 10
-        cout << n << " "; // Выводим значение элемента
-    }
+    cout << "Hello world!" << endl;
     return 0;
 }
